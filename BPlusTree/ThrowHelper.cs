53214a6db9be2fd295c30e0f96267c9456d961f3
Michael Adelson <mike.adelson314@gmail.com>
2022-02-26 18:53:14 -0500

    Move code out of test project

---
+++ BPlusTree/ThrowHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPlusTree.Tests
+{
+    internal static class ThrowHelper
+    {
+        [DoesNotReturn]
+        public static void ThrowArgumentOutOfRange() =>
+            throw new ArgumentOutOfRangeException("index", "Index was out of range. Must be non-negative and less than the size of the collection.");
+
+        [DoesNotReturn]
+        public static void ThrowArgumentNull(string parameterName) =>
+            throw new ArgumentNullException(parameterName);
+    }
+}
