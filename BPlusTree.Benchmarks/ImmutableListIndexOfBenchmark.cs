a2e000ee7dcd0be21973d2e2462f253655868004
Michael Adelson <mike.adelson314@gmail.com>
2022-04-13 07:57:46 -0400

    IndexOf benchmark make sure value isn't in the list

--- BPlusTree.Benchmarks/ImmutableListIndexOfBenchmark.cs
+++ BPlusTree.Benchmarks/ImmutableListIndexOfBenchmark.cs
@@ -23,23 +23,27 @@ namespace BPlusTree.Benchmarks
 
         [GlobalSetup(Target = nameof(ImmutableList))]
         public void SetUpImmutableList() =>
-            _immutableList = System.Collections.Immutable.ImmutableList.CreateRange(ValuesGenerator.UniqueValues<T>(Size));
+            _immutableList = System.Collections.Immutable.ImmutableList.CreateRange(GetListValues());
 
         [Benchmark(Baseline = true)]
         public int ImmutableList() => _immutableList!.IndexOf(default!, 0, Size, null);
 
         [GlobalSetup(Target = nameof(ArrayBasedImmutableList))]
         public void SetUpArrayBasedImmutableList() =>
-            _arrayBasedImmutableList = ArrayBasedBPlusTreeImmutableList.CreateRange(ValuesGenerator.UniqueValues<T>(Size));
+            _arrayBasedImmutableList = ArrayBasedBPlusTreeImmutableList.CreateRange(GetListValues());
 
         [Benchmark]
         public int ArrayBasedImmutableList() => _arrayBasedImmutableList!.IndexOf(default!, 0, Size, null);
 
         [GlobalSetup(Target = nameof(TunnelVisionImmutableList))]
         public void SetUpTunnelVisionImmutableList() =>
-           _tunnelVisionImmutableList = ImmutableTreeList.CreateRange(ValuesGenerator.UniqueValues<T>(Size));
+           _tunnelVisionImmutableList = ImmutableTreeList.CreateRange(GetListValues());
 
         [Benchmark]
         public int TunnelVisionImmutableList() => _tunnelVisionImmutableList!.IndexOf(default!, 0, Size, null);
+
+        private IEnumerable<T> GetListValues() =>
+            // filter out default(T) since that's the search value
+            ValuesGenerator.UniqueValues<T>(Size + 1).Where(i => !EqualityComparer<T>.Default.Equals(i, default)).Take(Size);
     }
 }
